1ff4f74bef526ae0ef812f9c567a31fa57995c15
pagalbhanu <115147097+pagalbhanu@users.noreply.github.com>
2022-10-07 15:16:46 +0530

    durfh

---
+++ d.php
@@ -0,0 +1,8 @@
+<html>
+    <body>
+        welcome <?php echo $_POST["name"];?>
+        <?php echo $_POST["sname"];?>
+        <?php echo "<br>"?>
+        your roll no is: <?php echo $_POST["rollno"];?>
+    </body>
+</html>
